bdb3fb896bd906602079ed3f128f8961f6a285db
Negative <79591062+AppleBlu@users.noreply.github.com>
2022-09-17 23:40:09 +0100

    Update metro.py

--- metro.py
+++ metro.py
@@ -1,23 +1,24 @@
-class MetroCard():
+import customer
+class MetroCard:
     tickets_for_destination = {'Midtown': 10, 'Central Park': 4, 'Metropolitan Museum of Art': 1}
     price_of_ticket = {'Midtown': 4.32, 'Central Park': 2.43, 'Metropolitan Museum of Art': 8.64}
+    destinations = list(tickets_for_destination.keys())
+    prices = list(price_of_ticket.values())
+    card_fee = 1
 
-    def __init__(self):
-        self.destination = 'Midtown'
-        self.price = MetroCard.price_of_ticket.get(self.destination)
-        self.tickets = MetroCard.tickets_for_destination.get(self.destination)
+    def print_locations():
+        for key, value in MetroCard.price_of_ticket.items():
+            print(f"{key}: £{value}")
 
-    def get_price():
-        prices = MetroCard.price_of_ticket
-        for key, value in prices.items():
-            return value
-    
-    def get_destinations():
+    def un_capatalise_destinations():
+        destinations_lower = []
+        for i in MetroCard.destinations:
+            destinations_lower.append(i.lower())
+        return destinations_lower
+
+   
+    def print_destination_to_tickets():
         for key, value in MetroCard.tickets_for_destination.items():
-            return key
+            print(f"There are {value} tickets avalible for destination: {key}")
 
-    def __repr__(self):
-        
-        return '''The price to travel to {destination} is {price}
-There are {tickets} remaining tickets to travel to {destination}.
-        '''.format(destination = self.destination, tickets = self.tickets, price = self.price)
+    
